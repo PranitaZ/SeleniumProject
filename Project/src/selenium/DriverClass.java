5a0aebf8bedba21e4014baf401cf402f92ee7584
ibm <ibm@przatale.in.ibm.com>
2017-05-27 12:14:45 +0530

    added a driver

--- Project/src/selenium/DriverClass.java
+++ Project/src/selenium/DriverClass.java
@@ -1,10 +1,17 @@
 package selenium;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverClass {
 
 	WebDriver wd = new FirefoxDriver();
+	
+	@Test
+	public void launch(){
+		
+		wd.get("https://www.google.co.in/");
+	}
 	
 }
